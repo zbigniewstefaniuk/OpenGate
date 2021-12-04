95f7d7cc2256c921cc01b6c309ac7e5f93360113
Zbigniew Stefaniuk <zbyszek791@hotmail.com>
2021-12-04 13:40:01 +0100

    init commit

---
+++ android/app/src/main/java/com/opengate/MainActivity.java
@@ -0,0 +1,22 @@
+package com.opengate;
+
+import com.facebook.react.ReactActivity;
+import com.github.wumke.RNImmediatePhoneCall.RNImmediatePhoneCallPackage; 
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "OpenGate";
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+      RNImmediatePhoneCallPackage.onRequestPermissionsResult(requestCode, permissions, grantResults); // very important event callback
+      super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+  }  
+}
